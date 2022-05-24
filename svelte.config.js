9084efc424b5e351281face5b82bdb984c79338d
Shadofer <shadowrlrs@gmail.com>
2022-05-25 02:43:45 +0300

    Add type definitions (#11)
    * Update gitignore paths
    
    * Use Svelte-Kit for packaging, update structure
    
    * Update tsconfig
    
    * Update npmignore

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+      package: {
+        exports: filepath => {
+          return filepath === 'index.js' || filepath.endsWith('.json')
+        }
+      }
+    }
+  }
+  
+  export default config
